5ddf34afb0af66e57d479a17d03de615d2d1aed8
BGH76 <75952591+BGH76@users.noreply.github.com>
2021-09-20 14:46:48 -0400

    initial commit

--- js/site.js
+++ js/site.js
@@ -1,4 +1,36 @@
-// Call Hello World
-function HelloWorld(){
-    alert("Hello World");
+
+document.getElementById("btnCheck").addEventListener("click", runTest);
+let alertBox = document.getElementById("alert").classList;
+let msg = document.getElementById("msg");
+let alertHeading = document.getElementById("alertHeading");
+
+function runTest(){
+    let phrase = document.getElementById("userPhrase").value;
+
+    // Get phrase in lowercase with spaces and special characters removed
+    phrase = phrase.toLowerCase();
+    let regrex = /[^a-z0-9]/gi;
+    phrase = phrase.replace(regrex, "");
+
+    // Reverse phrase and store in variable temp
+    let temp = "";
+    for(let i = phrase.length-1; i >= 0; i--){
+        temp += phrase[i];
+    }
+
+    // Compare characters of phrase and reversed phrase
+    for(let j = 0; j < phrase.length; j++){
+        if(phrase[j] != temp[j]){
+            alertBox.remove("alert-success");
+            alertBox.add("alert-danger");
+            alertHeading.innerHTML = "Sorry! Your phrase is not a palindrome";
+            msg.innerHTML = `${phrase} is not a palindrome`;
+            alertBox.remove("invisible");
+            return;
+        }    
+    }
+
+    msg.innerHTML = `Your phrase ${phrase} is a palindrome.`;
+    alertBox.remove("invisible");
+    alertHeading.innerHTML = "Terrific! Your phrase is a <strong>Palindrome</strong>"; 
 }
